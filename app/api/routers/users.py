6b09b9d487a62c5cb2d974c8c6a6c9f14636f6c9
Miguel Angel <miguelangelgarb@gmail.com>
2025-09-04 13:13:34 -0500

    Backend v.1.2.0

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
 from pydantic import BaseModel
+from typing import Optional, List
+
 from app.api.database import SessionLocal
 from app.api.models.users import User 
 from app.api.utils import hash_password
@@ -12,6 +15,10 @@ class UserCreate(BaseModel):
     username: str
     email: str
     password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str 
 
 def get_db():
     db = SessionLocal()
@@ -32,4 +39,20 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+@router.get("/", response_model=UserCreate)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == user.username).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found!")
+
+    if user: 
+        return User.password
+    
+    passUser = hash_password(user.password)
+
+    if passUser == User.password:
+        raise HTTPException(status_code=200, detail="Login successful")
+
 
